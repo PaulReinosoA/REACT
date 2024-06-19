f1ddbddcfa4d3821faeec2824dd94eeb1bcd8e5b
Washington Reinoso <Washington.Reinoso@Portales.Local>
2024-06-19 17:59:56 -0500

    193-CreandoUnSegundoRouter

--- Curso-React/07-heroes-spa/src/router/AppRouter.jsx
+++ Curso-React/07-heroes-spa/src/router/AppRouter.jsx
@@ -1,8 +1,8 @@
 import { Navigate, Route, Routes } from 'react-router';
-import { MarvelPage } from '../heroes/pages/MarvelPage';
-import { DcPage } from '../heroes/pages/DcPage';
-import { LoginPage } from '../auth/pages/LoginPage';
+import { MarvelPage } from '../heroes';
+import { LoginPage } from '../auth';
 import { Navbar } from '../ui';
+import { DcPage } from '../heroes';
 
 export const AppRouter = () => {
   return (
