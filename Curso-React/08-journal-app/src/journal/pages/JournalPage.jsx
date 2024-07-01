71ff184252632b95483a93cafb18d5c17f8541ce
Washington Reinoso <Washington.Reinoso@Portales.Local>
2024-07-01 17:39:03 -0500

    261 - como usar valores de store

--- Curso-React/08-journal-app/src/journal/pages/JournalPage.jsx
+++ Curso-React/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,14 +1,28 @@
-import { Typography } from '@mui/material';
 import React from 'react';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 import { JournalLayout } from '../../auth/layout/JournalLayout';
-import { NothingSelectedView } from '../view/NothingSelectedView';
-import { NoteView } from '../view/NoteView';
+import { /* NoteView , */ NothingSelectedView } from '../view';
 
 export const JournalPage = () => (
   <>
     <JournalLayout>
       {/* <NothingSelectedView /> */}
-      <NoteView />
+      {/* <NoteView /> */}
+      <NothingSelectedView />
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   </>
 );
