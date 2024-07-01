9d36ee43f840181bfa62d0a9587745f9a32a88bc
PaulReinosoA <164814520+PaulReinosoA@users.noreply.github.com>
2024-06-30 23:45:16 -0500

    253 - Imagenes de Galeria en journal app

--- Curso-React/08-journal-app/src/journal/pages/JournalPage.jsx
+++ Curso-React/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import { Typography } from '@mui/material';
 import React from 'react';
 import { JournalLayout } from '../../auth/layout/JournalLayout';
+import { NothingSelectedView } from '../view/NothingSelectedView';
+import { NoteView } from '../view/NoteView';
 
 export const JournalPage = () => (
   <>
     <JournalLayout>
-      <Typography>
-        JournalPage sdcsdcvsdv JournalPage sdcsdcvsdv JournalPage sdcsdcvsdv
-        JournalPage sdcsdcvsdv JournalPage sdcsdcvsdv JournalPage sdcsdcvsdv{' '}
-      </Typography>
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   </>
 );
