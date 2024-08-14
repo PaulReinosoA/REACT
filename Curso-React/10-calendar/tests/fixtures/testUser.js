f2e626d30578ce7772e31cee0a161ba695ee1507
PaulReinosoA <164814520+PaulReinosoA@users.noreply.github.com>
2024-08-13 23:26:17 -0500

    445 - Calendar fixtures

---
+++ Curso-React/10-calendar/tests/fixtures/testUser.js
@@ -0,0 +1,6 @@
+export const testUserCredentials = {
+  email: 'test@google.com',
+  password: '123456',
+  uid: '66b81fa69c5694d572b6daa1',
+  name: 'Test User',
+};
--- Curso-React/10-calendar/tests/store/auth/authSlice.test.js
+++ Curso-React/10-calendar/tests/store/auth/authSlice.test.js
@@ -1,3 +1,57 @@
+import {
+  authSlice,
+  clearErrorMessage,
+  onChecking,
+  onLogin,
+  onLogOut,
+} from '../../../src/store/auth/authSlice';
+import { AuthenticatedState, initialState } from '../../fixtures/authStates';
+import { testUserCredentials } from '../../fixtures/testUser';
+
 describe('pruebas sobre authSlice.js', () => {
-  test('debe de regresar el estado inicial', () => {});
+  test('debe de regresar el estado inicial', () => {
+    expect(authSlice.getInitialState()).toEqual(initialState);
+  });
+
+  test('debe de realizar un logIn ', () => {
+    const state = authSlice.reducer(initialState, onLogin(testUserCredentials));
+    // console.log(state);
+    expect(state).toEqual({
+      status: 'Authenticated',
+      user: testUserCredentials,
+      errorMessage: undefined,
+    });
+  });
+
+  test('debe de realizar un logOut  ', () => {
+    const errorMessage = 'sesion cerrada';
+    const state = authSlice.reducer(AuthenticatedState, onLogOut(errorMessage));
+    // console.log(state);
+    expect(state).toEqual({
+      status: 'not-Authenticated',
+      user: {},
+      errorMessage: errorMessage,
+    });
+  });
+
+  test('debe de limpiar el mensaje de error', () => {
+    const errorMessage = 'sesion cerrada';
+    const state = authSlice.reducer(AuthenticatedState, onLogOut(errorMessage));
+    const newState = authSlice.reducer(state, clearErrorMessage());
+    expect(newState.errorMessage).toBe(undefined);
+  });
+
+  test('debe de realizar un checking', () => {
+    const errorMessage = 'sesion cerrada';
+    const state = authSlice.reducer(AuthenticatedState, onLogOut(errorMessage));
+    const newState = authSlice.reducer(state, onChecking());
+
+    // console.log(newState);
+
+    expect(newState).toEqual({
+      errorMessage: undefined,
+      status: 'checking',
+      user: {},
+    });
+  });
 });
