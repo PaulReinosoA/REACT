0a561fdfdffdd879b64fcbb794ea42f33f5baf05
Washington Reinoso <Washington.Reinoso@Portales.Local>
2024-08-14 17:37:07 -0500

    445 - 3:23 calendar test

---
+++ Curso-React/10-calendar/tests/fixtures/calendarStates.js
@@ -0,0 +1,16 @@
+export const events = [
+  {
+    id: '1',
+    start: new Date('2022-10-27 13:00:00'),
+    end: new Date('2022-10-27 15:00:00'),
+    title: 'cumpleaños de Paul',
+    notes: 'alguna anotacion de paul',
+  },
+  {
+    id: '2',
+    start: new Date('2022-12-27 13:00:00'),
+    end: new Date('2022-12-27 15:00:00'),
+    title: 'cumpleaños de Nicole',
+    notes: 'alguna anotacion de Nicole',
+  },
+];
