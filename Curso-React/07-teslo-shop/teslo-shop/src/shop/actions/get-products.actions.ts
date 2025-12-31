90cde91aa5e6945dbb57f4967cc87751c663833c
REINOSOW <wreinoso@solidario.fin.ec>
2025-12-31 10:57:09 -0500

    285 - filtros por genero y talla
    tarea

--- Curso-React/07-teslo-shop/teslo-shop/src/shop/actions/get-products.actions.ts
+++ Curso-React/07-teslo-shop/teslo-shop/src/shop/actions/get-products.actions.ts
@@ -1,13 +1,24 @@
 import { tesloApi } from '@/api/teslo-api';
 import type { ProductsResponse } from '@/interfaces/products.response';
 
-export const getProductsActions = async ():Promise<ProductsResponse> => {
-  const { data } = await tesloApi.get<ProductsResponse>('/products');  
+interface Options {
+  limit?: number | string;
+  offset?: number | string;
+}
+
+export const getProductsActions = async (
+  options: Options
+): Promise<ProductsResponse> => {
+  const { limit, offset } = options;
+  const { data } = await tesloApi.get<ProductsResponse>('/products', {
+    params: { limit, offset },
+  });
+
   const productsWithImagesUrl = data.products.map((product) => ({
     ...product,
-    images: product.images.map((image) => 
-      `${import.meta.env.VITE_API_URL}/files/product/${image}`
+    images: product.images.map(
+      (image) => `${import.meta.env.VITE_API_URL}/files/product/${image}`
     ),
   }));
-  return {  ...data, products: productsWithImagesUrl };
+  return { ...data, products: productsWithImagesUrl };
 };
--- Curso-React/07-teslo-shop/teslo-shop/src/shop/hooks/useProducts.tsx
+++ Curso-React/07-teslo-shop/teslo-shop/src/shop/hooks/useProducts.tsx
@@ -1,9 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import { getProductsActions } from '../actions/get-products.actions';
+import { useSearchParams } from 'react-router';
 
 export const useProducts = () => {
+  const [searchParams] = useSearchParams();
+  const limit = searchParams.get('limit') || 9;
+  const page = searchParams.get('page') || 1;
+  const offset = Number(page) * Number(limit);
+
   return useQuery({
-    queryKey: ['products'],
-    queryFn: getProductsActions,
+    queryKey: ['products', { limit, offset }],
+    queryFn: () =>
+      getProductsActions({
+        limit: isNaN(+limit) ? 9 : limit,
+        offset: isNaN(offset) ? 0 : offset,
+      }),
+    staleTime: 1000 * 60 * 5, // 5 minutes
   });
 };
